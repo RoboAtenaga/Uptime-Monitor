99e344b987771a509f4fd1b4a1558bbce6db9fcb
Robo Ruth Atenaga <roboatenaga@gmail.com>
2019-01-22 16:10:23 +0700

    Added a CLI for admin purposes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 // Declare the append
 var app = {};
@@ -14,6 +15,11 @@ app.init = function(){
 	// Start the server and workers
 	server.init();
 	workers.init();
+
+	// Start the CLI last, so that the logs from the server etc will happen 1st
+	setTimeout(function(){
+		cli.init();
+	},50);
 };
 
 // Execute the app
